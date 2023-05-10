5792adab50880edce96a18d239b1765075f49d80
juan Sterba <sjcexe@gmail.com>
2023-05-10 17:16:40 -0300

    solucionado el findByName

--- src/main/java/com/itec1api/biblioteca/controllers/BookController.java
+++ src/main/java/com/itec1api/biblioteca/controllers/BookController.java
@@ -4,10 +4,7 @@ import com.itec1api.biblioteca.DTO.BookResponseDTO;
 import com.itec1api.biblioteca.services.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,8 +23,8 @@ public class BookController {
 //        return new ResponseEntity<>(responseDTOS, HttpStatus.OK);
 //    }
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public ResponseEntity<List<BookResponseDTO>> getOne(@PathVariable String name) {
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<BookResponseDTO>> getOne(@RequestParam String name) {
         List<BookResponseDTO> responseDTOS = bookService.findByName(name);
         return new ResponseEntity<>(responseDTOS, HttpStatus.OK);
     }
