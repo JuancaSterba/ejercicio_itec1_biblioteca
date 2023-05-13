e08edc2768573ef9b4dd884d4d40241b21e75f6d
juan Sterba <sjcexe@gmail.com>
2023-05-13 01:51:23 -0300

    se agrega JPA y H2

--- src/main/java/com/itec1api/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/itec1api/biblioteca/BibliotecaApplication.java
@@ -2,8 +2,10 @@ package com.itec1api.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class BibliotecaApplication {
 
 	public static void main(String[] args) {
